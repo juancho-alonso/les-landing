3c671832104f8d7cb78694f1145e43217941adb4
juancho-alonso <juan.agustin.alonso@gmail.com>
2020-05-04 20:50:33 +1000

    Adjusted behavior on form and navigate

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 function navigate(id) {
     const dataId = id.getAttribute("data-id");
-    //document.getElementById(dataId).scrollIntoView({behavior: 'smooth'});
+    document.getElementById(dataId).scrollIntoView({behavior: 'smooth'});
     document.getElementById(dataId).scrollTop -= 10;
     
 };
 
+var inputs = document.getElementsByClassName("inputs");
+
+function checkInputs(){
+    for (i = 0; i < inputs.length; i++) {    
+        if (inputs[i].value === ""){
+            inputs[i].style.boxShadow = "0px 0px 2px 2px rgba(255,0,0,1)";
+        } else if (inputs[i].value !== ""){
+            inputs[i].style.boxShadow = "none";
+        }
+    }
+}
+
+var labels = document.getElementsByClassName('labels');
+
+document.getElementById("send-btn").addEventListener("click", missingInfo);
+
+function missingInfo() {
+    for (i = 0; i < inputs.length; i++) {    
+        if (inputs[i].value === ""){
+           // labels[i].insertAdjacentText('afterend', "Please fill this field!");
+        } else if (labels[i].value !== ""){
+           // labels[i].removeChild(labels[i].childNodes[0]);
+        }
+    }
+}
+
+label.insertAdjacentText('afterend', 'My Plain Text..');
